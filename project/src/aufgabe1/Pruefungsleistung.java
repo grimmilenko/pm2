14e4ddc9ecf13f49f87482c390d6a1fab51e585e
Nico Grimm <nico.grimm@haw-hamburg.de>
2015-10-12 20:16:18 +0200

    adding class Pruefungsleistung

---
+++ project/src/aufgabe1/Pruefungsleistung.java
@@ -0,0 +1,15 @@
+package aufgabe1;
+
+public class Pruefungsleistung {
+
+	private String modul;
+	private int note;
+
+	public String getModul() {
+		return modul;
+	}
+
+	public int getNote() {
+		return note;
+	}
+}
