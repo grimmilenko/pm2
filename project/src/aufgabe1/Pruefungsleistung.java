56ec159cd6b11dc4a85f9c0aa51f6bc3935ec3ec
Nico Grimm <nico.grimm@haw-hamburg.de>
2015-10-14 09:37:10 +0200

    added comments

--- project/src/aufgabe1/Pruefungsleistung.java
+++ project/src/aufgabe1/Pruefungsleistung.java
@@ -1,14 +1,38 @@
+/**
+ * Praktikum PM2, WS 15/16
+ * Nico Grimm (nico.grimm@haw-hamburg.de)
+ * Marco Colbow (marco.colbow@haw-hamburg.de)
+ * Aufgabe 1
+ */
+
 package aufgabe1;
 
+/**
+ * Klasse zum darstellen abgelegter Pruefungen mit Namen des Moduls und
+ * zugehoeriger Note
+ * 
+ * @author Nico Grimm und Marco Colbow
+ *
+ */
 public class Pruefungsleistung {
 
 	private String modul;
 	private int note;
 
+	/**
+	 * Getter
+	 * 
+	 * @return Gibt den Namen des Moduls zurueck
+	 */
 	public String getModul() {
 		return modul;
 	}
 
+	/**
+	 * Getter
+	 * 
+	 * @return Gibt die Note als int zurueck
+	 */
 	public int getNote() {
 		return note;
 	}
--- project/src/aufgabe1/Student.java
+++ project/src/aufgabe1/Student.java
@@ -1,24 +1,62 @@
+/**
+ * Praktikum PM2, WS 15/16
+ * Nico Grimm (nico.grimm@haw-hamburg.de)
+ * Marco Colbow (marco.colbow@haw-hamburg.de)
+ * Aufgabe 1
+ */
+
 package aufgabe1;
 
+/**
+ * Klasse zur Darstellung eines Studenten
+ * 
+ * @author Nico Grimm und Marco Colbow
+ *
+ */
 public class Student implements Comparable<Student> {
 	private String vorname;
 	private String nachname;
 	private int matrikelnummer;
 
+	/**
+	 * Konstruktor der Klasse Student
+	 * 
+	 * @param vorname
+	 *            Vorname eines Studenten
+	 * @param nachname
+	 *            Nachname eines Studenten
+	 * @param matrikelnummer
+	 *            Matrikelnummer eines Studenten
+	 */
 	public Student(String vorname, String nachname, int matrikelnummer) {
 		this.vorname = vorname;
 		this.nachname = nachname;
 		this.matrikelnummer = matrikelnummer;
 	}
 
+	/**
+	 * Getter
+	 * 
+	 * @return Gibt den Vornamen zurueck
+	 */
 	public String getVorname() {
 		return vorname;
 	}
 
+	/**
+	 * Getter
+	 * 
+	 * @return Gibt den Nachnamen zurueck
+	 */
 	public String getNachname() {
 		return nachname;
 	}
 
+	/**
+	 * Getter
+	 * 
+	 * @return Gibt die Matrikelnummer zurueck
+	 */
 	public int getMatrikelnummer() {
 		return matrikelnummer;
 	}
