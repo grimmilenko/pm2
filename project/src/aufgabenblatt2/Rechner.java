b92f4c91ae490f9364d4a5165e74fe108f43a36d
Nico Grimm <nico.grimm@haw-hamburg.de>
2015-10-21 12:39:42 +0200

    initial commit

---
+++ project/src/aufgabenblatt2/Rechner.java
@@ -0,0 +1,21 @@
+/**
+ * Praktikum PM2, WS 15/16
+ * Nico Grimm (nico.grimm@haw-hamburg.de)
+ * Marco Colbow (marco.colbow@haw-hamburg.de)
+ * Aufgabe 2
+ */
+
+package aufgabenblatt2;
+
+public class Rechner {
+
+	public enum Operation {
+		ADDITION, SUBTRAKTION, MULTIPLIKATION, DIVISION
+	}
+
+	public double berechne(Operation operation, double zahl1, double zahl2) {
+		double ergebnis = 0;
+		return ergebnis;
+	}
+
+}
