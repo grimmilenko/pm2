2aaa1fdcda5b7508c6057c85dcf3a84d626b4c57
Nico Grimm <nico.grimm@haw-hamburg.de>
2015-10-29 11:08:57 +0100

    adding

--- project/src/aufgabenblatt2/Rechner.java
+++ project/src/aufgabenblatt2/Rechner.java
@@ -7,6 +7,9 @@
 
 package aufgabenblatt2;
 
+import java.util.*;
+import java.util.function.BinaryOperator;
+
 /**
  * Klasse zur Repraesentation eines einfachen Rechners
  * 
@@ -25,9 +28,57 @@ public class Rechner {
 		ADDITION, SUBTRAKTION, MULTIPLIKATION, DIVISION
 	}
 
-	public double berechne(Operation operation, double zahl1, double zahl2) {
-		double ergebnis = 0;
-		return ergebnis;
+	private Map<Operation, BinaryOperator<Double>> map = new HashMap<Operation, BinaryOperator<Double>>() {
+		// put(Operation.ADDITION, DoubleDoubleZuDouble add = (wert1, wert2) ->
+		// {
+		// return wert1 + wert2;};);
+	};
+
+	/**
+	 * Methode zum berechnen zweier Zahlen mit einer bestimmten Rechenoperation
+	 * 
+	 * @param operation
+	 *            Rechenoperation, die ausgefuehrt werden soll
+	 * @param zahl1
+	 *            Erste Zahl
+	 * @param zahl2
+	 *            Zweite Zahl
+	 * @return Gibt das Ergebnis der Berechnung wieder
+	 * @throws Exception
+	 *             Bei Division darf nicht durch 0 geteilt werden und bei
+	 *             unbekannter Rechenoperation
+	 */
+	public double berechne(Operation operation, double zahl1, double zahl2)
+			throws Exception {
+		switch (operation) {
+		case ADDITION:
+			DoubleDoubleZuDouble add = (wert1, wert2) -> {
+				return wert1 + wert2;
+			};
+			return add.werteAus(zahl1, zahl2);
+		case SUBTRAKTION:
+			DoubleDoubleZuDouble sub = (wert1, wert2) -> {
+				return wert1 - wert2;
+			};
+			return sub.werteAus(zahl1, zahl2);
+		case MULTIPLIKATION:
+			DoubleDoubleZuDouble mul = (wert1, wert2) -> {
+				return wert1 * wert2;
+			};
+			return mul.werteAus(zahl1, zahl2);
+		case DIVISION:
+			DoubleDoubleZuDouble div = (wert1, wert2) -> {
+				return wert1 / wert2;
+			};
+			if (zahl2 == 0) {
+				throw new Exception("Es darf nicht durch 0 geteilt werden!");
+			}
+			return div.werteAus(zahl1, zahl2);
+		default:
+			throw new Exception(
+					"Die gewuenschte Rechenoperation ist nicht Verfuegbar!");
+		}
+
 	}
 
 }
