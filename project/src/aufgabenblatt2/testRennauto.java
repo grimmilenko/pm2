d0999fc3fe021b6ed9a1485703e4170878bd26e8
Nico Grimm <nico.grimm@haw-hamburg.de>
2015-11-06 14:24:46 +0100

    adding testRennauto

---
+++ project/src/aufgabenblatt2/testRennauto.java
@@ -0,0 +1,27 @@
+/**
+ * Praktikum PM2, WS 15/16
+ * Nico Grimm (nico.grimm@haw-hamburg.de)
+ * Marco Colbow (marco.colbow@haw-hamburg.de)
+ * Aufgabe 2
+ */
+
+package aufgabenblatt2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Test-Klasse
+ * 
+ * @author Nico
+ *
+ */
+public class testRennauto {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
