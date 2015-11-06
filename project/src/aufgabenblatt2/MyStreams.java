446da2d9f954b84baacc1ff5c8fe2ecbf6683152
Nico Grimm <nico.grimm@haw-hamburg.de>
2015-11-06 08:54:59 +0100

    adding streams

---
+++ project/src/aufgabenblatt2/MyStreams.java
@@ -0,0 +1,39 @@
+/**
+ * Praktikum PM2, WS 15/16
+ * Nico Grimm (nico.grimm@haw-hamburg.de)
+ * Marco Colbow (marco.colbow@haw-hamburg.de)
+ * Aufgabe 2
+ */
+
+package aufgabenblatt2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Klasse zum bearbeiten eines String-Arrays mit Hilfe von Streams
+ * 
+ * @author Nico
+ *
+ */
+public class MyStreams {
+
+	public String kuerzen(String string) {
+		if (string.length() <= 8) {
+			return string;
+		} else {
+			return string.substring(0, 8);
+		}
+	}
+
+	public List<String> streamVerarbeitung(String[] array) {
+		return Arrays
+				.stream(array)
+				.filter(element -> element != null)
+				.map(element -> kuerzen(element.toUpperCase().trim()
+						.replace("Ä", "AE").replace("Ö", "OE")
+						.replace("Ü", "UE").replace("ß", "SS")))
+				.collect(Collectors.toList());
+	}
+}
---
+++ project/src/aufgabenblatt2/testMyStreams.java
@@ -0,0 +1,35 @@
+/**
+ * Praktikum PM2, WS 15/16
+ * Nico Grimm (nico.grimm@haw-hamburg.de)
+ * Marco Colbow (marco.colbow@haw-hamburg.de)
+ * Aufgabe 2
+ */
+
+package aufgabenblatt2;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+/**
+ * Test-Klasse
+ * 
+ * @author Nico
+ *
+ */
+public class testMyStreams {
+
+	@Test
+	public void test() {
+		String[] array = { "Eingabe", "Äußeres", null, "Strassen-Feger",
+				"ein Haus" };
+		MyStreams test = new MyStreams();
+		ArrayList<String> erwartet = new ArrayList<String>(Arrays.asList(
+				"EINGABE", "AEUSSERE", "STRASSEN", "EIN HAUS"));
+		assertEquals(erwartet, test.streamVerarbeitung(array));
+	}
+
+}
