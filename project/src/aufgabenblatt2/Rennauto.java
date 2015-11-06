0d14e443bd924bfb3e0778bee0896e0d5c8f399c
Nico Grimm <nico.grimm@haw-hamburg.de>
2015-11-06 12:32:22 +0100

    updating Rennauto

--- project/src/aufgabenblatt2/Rennauto.java
+++ project/src/aufgabenblatt2/Rennauto.java
@@ -13,6 +13,42 @@ package aufgabenblatt2;
  * @author Nico
  *
  */
-public class Rennauto {
+public class Rennauto extends Thread {
+	// private double avgSpeed = randomRange(0.8, 1.2);
+	private static int UniqueId = 0;
+	private int id;
+	private final static double STRECKE = 10.0;
 
+	public Rennauto() {
+		id = UniqueId++;
+	}
+
+	/**
+	 * Methode zur Erzeugung einer Zufallszahl in einem bestimmten Bereich
+	 * 
+	 * @param min
+	 *            minimale Zufallszahl
+	 * @param max
+	 *            maximale Zufallszahl
+	 * @return Zufallszahl zwischen min und max
+	 */
+	private double randomRange(double min, double max) {
+		double range = (max - min);
+		return (Math.random() * range) + min;
+	}
+
+	@Override
+	public void run() {
+		for (double strecke = 0.0; strecke <= STRECKE + 1; strecke = strecke
+				+ randomRange(0.8, 1.2)) {
+			System.err.println("Wagen " + id + ":" + strecke + "/" + STRECKE);
+		}
+	}
+
+	public static void main(String[] args) {
+		Rennauto test = new Rennauto();
+		Rennauto test1 = new Rennauto();
+		test.start();
+		test1.start();
+	}
 }
