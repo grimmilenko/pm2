5899980417ab39aadecc1d680d2f0c6e9bea2df4
Nico Grimm <nico.grimm@haw-hamburg.de>
2015-11-06 11:43:08 +0100

    adding Rennauto

---
+++ project/src/aufgabenblatt2/Rennauto.java
@@ -0,0 +1,18 @@
+/**
+ * Praktikum PM2, WS 15/16
+ * Nico Grimm (nico.grimm@haw-hamburg.de)
+ * Marco Colbow (marco.colbow@haw-hamburg.de)
+ * Aufgabe 2
+ */
+
+package aufgabenblatt2;
+
+/**
+ * Klasse zur Realisierung eines Rennautos
+ * 
+ * @author Nico
+ *
+ */
+public class Rennauto {
+
+}
