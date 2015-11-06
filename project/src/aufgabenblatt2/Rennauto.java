7305f81d64cea8a81c4ba44f349db1d1ebff7580
Nico Grimm <nico.grimm@haw-hamburg.de>
2015-11-06 14:22:51 +0100

    updating Rennauto

--- project/src/aufgabenblatt2/Rennauto.java
+++ project/src/aufgabenblatt2/Rennauto.java
@@ -14,9 +14,9 @@ package aufgabenblatt2;
  *
  */
 public class Rennauto extends Thread {
-	// private double avgSpeed = randomRange(0.8, 1.2);
 	private static int UniqueId = 0;
 	private int id;
+	private double gefahreneZeit = 0.0;
 	private final static double STRECKE = 10.0;
 
 	public Rennauto() {
@@ -39,16 +39,41 @@ public class Rennauto extends Thread {
 
 	@Override
 	public void run() {
-		for (double strecke = 0.0; strecke <= STRECKE + 1; strecke = strecke
-				+ randomRange(0.8, 1.2)) {
-			System.err.println("Wagen " + id + ":" + strecke + "/" + STRECKE);
+		double gefahreneStrecke = 0.0;
+		for (gefahreneStrecke = 0.0; gefahreneStrecke < STRECKE + 1; gefahreneStrecke++) {
+			System.err.println("Wagen " + id + ": " + gefahreneStrecke + "/"
+					+ STRECKE);
+			gefahreneZeit = gefahreneZeit + randomRange(0.8, 1.2);
+
 		}
 	}
 
 	public static void main(String[] args) {
 		Rennauto test = new Rennauto();
 		Rennauto test1 = new Rennauto();
+		Rennauto test2 = new Rennauto();
+		Rennauto test3 = new Rennauto();
+		Rennauto test4 = new Rennauto();
+		Rennauto test5 = new Rennauto();
 		test.start();
 		test1.start();
+		test2.start();
+		test3.start();
+		test4.start();
+		test5.start();
+
+		try {
+			test.join();
+			test1.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.format("Wagen 0: %.2f\n", test.gefahreneZeit);
+		System.out.format("Wagen 1: %.2f\n", test1.gefahreneZeit);
+		System.out.format("Wagen 2: %.2f\n", test2.gefahreneZeit);
+		System.out.format("Wagen 3: %.2f\n", test3.gefahreneZeit);
+		System.out.format("Wagen 4: %.2f\n", test4.gefahreneZeit);
+		System.out.format("Wagen 5: %.2f", test5.gefahreneZeit);
 	}
 }
