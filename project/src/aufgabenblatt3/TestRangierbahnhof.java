65f44e43adf968883bd2fdeda4d4435aa672e7e8
Nico Grimm <nico.grimm@haw-hamburg.de>
2015-11-29 10:59:16 +0100

    added comments

--- project/src/aufgabenblatt3/TestRangierbahnhof.java
+++ project/src/aufgabenblatt3/TestRangierbahnhof.java
@@ -1,9 +1,22 @@
+/**
+ * Praktikum PM2, WS 15/16
+ * Nico Grimm (nico.grimm@haw-hamburg.de)
+ * Marco Colbow (marco.colbow@haw-hamburg.de)
+ * Aufgabe 3
+ */
+
 package aufgabenblatt3;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * Test-Klasse zum testen der Funktionalitaeten des Rangierbahnhofes
+ * 
+ * @author Nico
+ *
+ */
 public class TestRangierbahnhof {
 
 	@Test
