29c38a38747f2eef859bfb1387452e6a7b4c2306
Nico Grimm <nico.grimm@haw-hamburg.de>
2015-11-29 23:40:22 +0100

    coding style

--- project/src/aufgabenblatt3/SimulationVisuell.java
+++ project/src/aufgabenblatt3/SimulationVisuell.java
@@ -4,6 +4,7 @@
  * Marco Colbow (marco.colbow@haw-hamburg.de)
  * Aufgabe 3
  */
+
 package aufgabenblatt3;
 
 import java.util.Observable;
@@ -26,81 +27,74 @@ import javafx.stage.Stage;
  * @author Nico, Marco
  *
  */
+public class SimulationVisuell extends Application implements Observer {
 
-public class SimulationVisuell extends Application implements Observer{
-	
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
-	
+
 	@Override
 	public void start(Stage stage) throws Exception {
-		
-		
+
 		GridPane gridpane = new GridPane();
-	    StackPane root = new StackPane();
+		StackPane root = new StackPane();
 		root.getChildren().add(gridpane);
 		stage.setTitle("Bahnhofssimulation");
-		stage.setScene(new Scene (root, 200, 300));
+		stage.setScene(new Scene(root, 200, 300));
 		Simulation sim = new Simulation();
-		sim.addObserver(this); 
+		sim.addObserver(this);
 		new Thread(sim).start();
-		
-		
-		try{
+
+		try {
 			Thread.sleep(3000);
 			stage.show();
 			addBahnhof(gridpane);
-		} catch(InterruptedException e){
+		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
-		
-		//public Task<Boolean> aufgabe = new Task<Boolean>();
-			
-			//protected Boolean call() throws Exception{
-				//int numberofSteps = 20;
-				//updateProgress(0,numberofSteps -1);
-			
-		
-		
-		
-		
+
+	// public Task<Boolean> aufgabe = new Task<Boolean>();
+
+	// protected Boolean call() throws Exception{
+	// int numberofSteps = 20;
+	// updateProgress(0,numberofSteps -1);
+
 	/**
 	 * Methode, die einen Bahnhof zeichnet
 	 * 
-	 * @param root das Fenster in dem gezeichnnet wird
-	 *  
-	 */	
-	private void addBahnhof(Pane root){
-		Path path =new Path();
-		MoveTo moveTo =new MoveTo();
+	 * @param root
+	 *            das Fenster in dem gezeichnnet wird
+	 * 
+	 */
+	private void addBahnhof(Pane root) {
+		Path path = new Path();
+		MoveTo moveTo = new MoveTo();
 		moveTo.setX(0);
 		moveTo.setY(0);
 		LineTo lineTo = new LineTo();
-		//lineTo.setX(0);
-		//lineTo.setY(0);
-		LineTo lineTo2 =new LineTo();
-		LineTo lineTo3 =new LineTo();
-		LineTo lineTo4 =new LineTo();
-		LineTo lineTo5 =new LineTo();
-		LineTo lineTo6 =new LineTo();
-		LineTo lineTo7 =new LineTo();
-		LineTo lineTo8 =new LineTo();
-		LineTo lineTo9 =new LineTo();
-		LineTo lineTo10 =new LineTo();
-		LineTo lineTo11 =new LineTo();
-		LineTo lineTo12 =new LineTo();
-		LineTo lineTo13 =new LineTo();
-		LineTo lineTo14 =new LineTo();
-		
+		// lineTo.setX(0);
+		// lineTo.setY(0);
+		LineTo lineTo2 = new LineTo();
+		LineTo lineTo3 = new LineTo();
+		LineTo lineTo4 = new LineTo();
+		LineTo lineTo5 = new LineTo();
+		LineTo lineTo6 = new LineTo();
+		LineTo lineTo7 = new LineTo();
+		LineTo lineTo8 = new LineTo();
+		LineTo lineTo9 = new LineTo();
+		LineTo lineTo10 = new LineTo();
+		LineTo lineTo11 = new LineTo();
+		LineTo lineTo12 = new LineTo();
+		LineTo lineTo13 = new LineTo();
+		LineTo lineTo14 = new LineTo();
+
 		lineTo.setX(100);
 		lineTo.setY(0);
 		lineTo2.setX(100);
 		lineTo2.setY(30);
-		//moveTo.setX(10);
-		//moveTo.setY(10);
+		// moveTo.setX(10);
+		// moveTo.setY(10);
 		lineTo3.setX(30);
 		lineTo3.setY(30);
 		lineTo4.setX(30);
@@ -119,10 +113,7 @@ public class SimulationVisuell extends Application implements Observer{
 		lineTo10.setY(110);
 		lineTo11.setX(0);
 		lineTo11.setY(110);
-		
-		
-		
-		
+
 		path.getElements().add(moveTo);
 		path.getElements().add(lineTo);
 		path.getElements().add(lineTo2);
@@ -138,27 +129,15 @@ public class SimulationVisuell extends Application implements Observer{
 		path.getElements().add(lineTo12);
 		path.getElements().add(lineTo13);
 		path.getElements().add(lineTo14);
-		
+
 		path.setTranslateY(30);
 		path.setStrokeWidth(3);
 		path.setStroke(Color.BLACK);
 		root.getChildren().add(path);
-		
 	}
-		
-		
-	
+
 	@Override
 	public void update(Observable arg0, Object arg1) {
-	addBahnhof(Pane root);
-		
-	
-		
-		
+		// addBahnhof(Pane root);
 	}
-	
-	
-
-	
-	
 }
