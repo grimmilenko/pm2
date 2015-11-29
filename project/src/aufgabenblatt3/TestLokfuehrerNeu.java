1d36029a43a6477a030b1f339ce2ad80f26e2ed0
Nico Grimm <nico.grimm@haw-hamburg.de>
2015-11-29 10:59:56 +0100

    initial commit

---
+++ project/src/aufgabenblatt3/TestLokfuehrerNeu.java
@@ -0,0 +1,14 @@
+package aufgabenblatt3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestLokfuehrerNeu {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
