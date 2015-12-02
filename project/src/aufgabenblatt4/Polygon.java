ab66948a6a0825d43024da37996aea6325def7c7
Nico Grimm <nico.grimm@haw-hamburg.de>
2015-12-02 19:08:41 +0100

    initial commit

---
+++ project/src/aufgabenblatt4/Polygon.java
@@ -0,0 +1,12 @@
+/**
+ * Praktikum PM2, WS 15/16
+ * Nico Grimm (nico.grimm@haw-hamburg.de)
+ * Marco Colbow (marco.colbow@haw-hamburg.de)
+ * Aufgabe 4
+ */
+
+package aufgabenblatt4;
+
+public class Polygon {
+
+}
