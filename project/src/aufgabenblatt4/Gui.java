b48859b2a0698796bff639023c7d507c780500e3
Nico Grimm <nico.grimm@haw-hamburg.de>
2016-01-01 13:14:48 +0100

    darstellung fehlt, buttons müssen noch funktionen bekommen

--- project/src/aufgabenblatt4/Gui.java
+++ project/src/aufgabenblatt4/Gui.java
@@ -10,14 +10,13 @@ package aufgabenblatt4;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.Tooltip;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -28,6 +27,16 @@ import javafx.stage.Stage;
  */
 public class Gui extends Application {
 
+	private Stage primaryStage;
+	private Pane pane;
+
+	/**
+	 * Breite und Hoehe der Anwendung
+	 */
+	private final int width, height;
+
+	private PolygonDarstellung polygonDarstellung;
+
 	/**
 	 * Programmstartpunkt
 	 */
@@ -35,70 +44,100 @@ public class Gui extends Application {
 		launch(args);
 	}
 
+	/**
+	 * Default-Konstruktor
+	 */
+	public Gui() {
+		height = 600;
+		width = 600;
+	}
+
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStagePar) {
+		primaryStage = primaryStagePar;
 		primaryStage.setTitle("Polygon-Zeicheneditor");
-		AnchorPane anchorPane = new AnchorPane();
-		PolygonDarstellung polygonDarstellung = new PolygonDarstellung(new PolygonModell());
-
-		// Bedienungsinterface rechte Ankerseite!
-		VBox bedienung = new VBox(5);
-		Button zeichneButton = new Button("Zeichne / Neues Polygon");
-		zeichneButton.setOnAction(event -> {
-			System.out.println("Polygon gezeichnet und neues erstellt");
-		});
-		bedienung.getChildren().add(zeichneButton);
-
-		// Tabelle
-		TableView<String> tabelle = new TableView<String>();
-		tabelle.setPrefHeight(150);
-		tabelle.setPrefWidth(250);
-		TableColumn<String, Polygon> tabellenSpalte = new TableColumn<String, Polygon>("Polygon");
-		tabelle.getColumns().add(tabellenSpalte);
-		bedienung.getChildren().add(tabelle);
-
-		// Befehlsbox
-		HBox befehlBox = new HBox(5);
-
-		// Eingabefeld
-		TextField befehlEingabe = new TextField();
-
-		// Hilfe-Tooltip
-		Button hilfeButton = new Button("?");
-		hilfeButton.setOnMouseEntered(event -> {
-			hilfeButton.setEffect(new DropShadow());
-		});
-		hilfeButton.setOnMouseExited(event -> {
-			hilfeButton.setEffect(null);
-		});
-		Tooltip t = new Tooltip("Mögliche Befehle!");
-		hilfeButton.setTooltip(t);
-
-		// Button zum abschicken des Befehls
-		Button eingabeButton = new Button("Eingabe");
-		befehlBox.getChildren().addAll(befehlEingabe, hilfeButton, eingabeButton);
-		eingabeButton.setOnAction(event -> {
-			System.out.println(befehlEingabe.getText());
-		});
-		bedienung.getChildren().add(befehlBox);
-
-		// Exit-Button
-		Button exitButton = new Button("Exit");
-		exitButton.setOnAction((ereignis) -> {
-			primaryStage.close();
-		});
-		bedienung.getChildren().add(exitButton);
-
-		// AnchorPane zusammensetzen
-		AnchorPane.setRightAnchor(bedienung, 10.0);
-		AnchorPane.setTopAnchor(bedienung, 10.0);
-		AnchorPane.setBottomAnchor(bedienung, 10.0);
-		anchorPane.getChildren().add(bedienung);
-		anchorPane.getChildren().add(polygonDarstellung.getPane());
-
-		// Stage anzeigen
-		primaryStage.setScene(new Scene(anchorPane, 500, 255));
+		pane = paneInit();
+		primaryStage.setScene(new Scene(pane, width, height));
 		primaryStage.show();
 	}
 
+	/**
+	 * Getter
+	 * 
+	 * @return Gibt die Pane zurueck
+	 */
+	public Pane getPane() {
+		return pane;
+	}
+
+	/**
+	 * Methode zum erstellen der GUI
+	 * 
+	 * @return Gibt die GUI als BorderPane zurueck
+	 */
+	private Pane paneInit() {
+		BorderPane pane = new BorderPane();
+		pane.setLeft(polygonTabelle());
+		pane.setCenter(zeichenflaeche());
+		pane.setBottom(befehlInput());
+		return pane;
+	}
+
+	/**
+	 * Methode zum erstellen der Befehlseingabe
+	 * 
+	 * @return Gibt die Befehlseingabe als BorderPane zurueck
+	 */
+	private Pane befehlInput() {
+		BorderPane pane = new BorderPane();
+		TextArea area = new TextArea();
+		area.setMaxHeight(50);
+		pane.setCenter(area);
+		pane.setBottom(befehlButtons());
+		return pane;
+	}
+
+	/**
+	 * Methode zum erstellen der Buttons fuer die Befehlseingabe
+	 * 
+	 * @return Gibt die Buttons in einer HBox zureuck
+	 */
+	private Pane befehlButtons() {
+		HBox pane = new HBox();
+		pane.setSpacing(10);
+		Button hilfe = new Button("?");
+		hilfe.setTooltip(new Tooltip("tooltip"));
+		Button befehl = new Button("Fuehre aus!");
+		Button fertig = new Button("Fertig");
+		Button close = new Button("Exit");
+		close.setOnAction(event -> {
+			primaryStage.close();
+		});
+		pane.getChildren().addAll(hilfe, befehl, fertig, close);
+		return pane;
+	}
+
+	/**
+	 * Methode zum erstellen der PolygonDarstellung
+	 * 
+	 * @return Gibt die PolygonDarstellung zurueck
+	 */
+	private Pane zeichenflaeche() {
+		StackPane pane = new StackPane();
+		polygonDarstellung = new PolygonDarstellung();
+		return pane;
+	}
+
+	/**
+	 * Methode zum erstellen einer Tabelle, die die gezeichneten Polygone
+	 * anzeigt
+	 * 
+	 * @return Gibt eine Tabelle als BorderPane zurueck
+	 */
+	private Pane polygonTabelle() {
+		BorderPane pane = new BorderPane();
+		TableView<Polygon> tablle = new TableView<Polygon>();
+		pane.setCenter(tablle);
+		return pane;
+	}
 }
--- project/src/aufgabenblatt4/PolygonDarstellung.java
+++ project/src/aufgabenblatt4/PolygonDarstellung.java
@@ -9,10 +9,8 @@ package aufgabenblatt4;
 
 import java.util.Observable;
 import java.util.Observer;
-
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Ellipse;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 
 /**
  * Klasse zur Darstellung der Polygone
@@ -20,53 +18,15 @@ import javafx.scene.shape.Ellipse;
  * @author grimmilenko
  *
  */
-public class PolygonDarstellung extends Pane implements Observer {
-	private PolygonModell modell;
-	private Pane pane = new Pane();
+public class PolygonDarstellung extends Canvas implements Observer {
+	private GraphicsContext gc;
 
-	/**
-	 * Konstruktor
-	 * 
-	 * @param modell
-	 *            Referenz auf das zu zeichnende PolygonModell
-	 */
-	public PolygonDarstellung(PolygonModell modell) {
-		this.modell = modell;
-		this.modell.addObserver(this);
-	}
-
-	private Ellipse zeichnePunkt(Double centerX, Double centerY) {
-		Ellipse punkt = new Ellipse(2, 2);
-		punkt.setCenterX(centerX);
-		punkt.setCenterY(centerY);
-
-		return punkt;
-
-	}
-
-	private void zeichne() {
-
-		for (int i = 0; i < modell.getListePolygone().size() - 1; i++) {
-			for (int j = 0; j < modell.getListePolygone().get(i).getListePunkte().size(); j++) {
-				pane.getChildren().add(zeichnePunkt(modell.getListePolygone().get(i).getXAtIndex(j),
-						modell.getListePolygone().get(i).getYAtIndex(j)));
-			}
-		}
-	}
-
-	/**
-	 * Getter
-	 * 
-	 * @return Gibt die gezeichnete Pane zurueck
-	 */
-	public Pane getPane() {
-		// TODO alles muss zusammengefuegt werden
-		zeichne();
-		return pane;
+	public PolygonDarstellung() {
+		super(350, 350);
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
-		// TODO was passiert beim update?
+		// TODO Auto-generated method stub
 	}
 }
