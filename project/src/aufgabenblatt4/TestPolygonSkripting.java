0183030cfa79f071f56c7f98e1e60e800e6d3d70
Nico Grimm <nico.grimm@haw-hamburg.de>
2016-01-03 12:06:55 +0100

    Tests müssen noch geschrieben werden

---
+++ project/src/aufgabenblatt4/TestPolygonSkripting.java
@@ -0,0 +1,19 @@
+/**
+ * Praktikum PM2, WS 15/16
+ * Nico Grimm (nico.grimm@haw-hamburg.de)
+ * Marco Colbow (marco.colbow@haw-hamburg.de)
+ * Aufgabe 4
+ */
+
+package aufgabenblatt4;
+
+/**
+ * Test-Klasse zum ueberpruefen der Funktionalitaet der Regulaeren Ausdruecke
+ * 
+ * @author Nico
+ *
+ */
+public class TestPolygonSkripting {
+	
+
+}
---
+++ project/src/aufgabenblatt4/TestPolygonTabelle.java
@@ -0,0 +1,18 @@
+/**
+ * Praktikum PM2, WS 15/16
+ * Nico Grimm (nico.grimm@haw-hamburg.de)
+ * Marco Colbow (marco.colbow@haw-hamburg.de)
+ * Aufgabe 4
+ */
+
+package aufgabenblatt4;
+
+/**
+ * Test-Klasse zum ueberpruefen der Funktionalitaet der Tabelle der Polygone
+ * 
+ * @author Nico
+ *
+ */
+public class TestPolygonTabelle {
+
+}
